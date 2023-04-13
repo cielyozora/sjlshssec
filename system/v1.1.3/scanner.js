cfff71053f4cf2d722a562ec3bbcb99784c4e2eb
cielyozora <49108900+cielyozora@users.noreply.github.com>
2023-04-13 03:56:35 -0700

    Add files via upload

---
+++ system/v1.1.3/scanner.js
@@ -0,0 +1,30 @@
+
+const beep = () => {
+  AUDIO.src = "sounds/beep.mp3";
+}
+
+var currentText = '';
+function onScanSuccess(qrCodeMessage) {
+    document.getElementById('result').innerHTML = '<span class="result">'+qrCodeMessage+'</span>';
+    if (currentText !== qrCodeMessage) {
+        currentText = qrCodeMessage;
+        beep();
+  }
+}
+
+const AUDIO = new Audio();
+AUDIO.autoplay = true;
+document.querySelector("#sound").addEventListener("click", () => {
+  beep();
+});
+
+function onScanError(errorMessage) {
+  //handle scan error
+}
+
+var html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader", { fps: 10, qrbox: 250});
+html5QrcodeScanner.render(onScanSuccess, onScanError);
+
+
+
